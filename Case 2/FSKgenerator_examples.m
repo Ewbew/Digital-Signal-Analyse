7f18754291f8cfa12ea4c80e0a399331daf8ede3
Otto Sejrskild Santesson <santessonen@gmail.com>
2025-10-18 11:15:41 +0200

    Opgave 2 mere eller mindre færdig

--- Case 2/FSKgenerator_examples.m
+++ Case 2/FSKgenerator_examples.m
@@ -1,5 +1,5 @@
 % Spectrogram example
-clear
+clear all; close all; clc;
 
 fstart = 1000; % transmission band frequency start
 fend = 2000; % transmission band frequency end
@@ -10,13 +10,15 @@ x = FSKgenerator('hasta la vista!', fstart, fend, Tsymbol, fs);
 
 soundsc(x, fs)
 
+
+%%
 subplot(211), plot(x)
 subplot(212), plot(abs(fft(x)))
 
 figure
-spectrogram(x, rectwin(500), 0, 500, fs) % WINDOW = rectwin(500), NOVERLAP = 0, NFFT = 500 (no zero-padding), Fs = 20000
+spectrogram(x, rectwin(500), 0, 500, fs, 'yaxis') % WINDOW = rectwin(500), NOVERLAP = 0, NFFT = 500 (no zero-padding), Fs = 20000
 
-
+%%
 % Shorter duration and wider band
 fstart = 500; % transmission band frequency start
 fend = 5000; % transmission band frequency end
@@ -24,31 +26,40 @@ Tsymbol = 0.005; % symbol duration in seconds
 x = FSKgenerator('hasta la vista!', fstart, fend, Tsymbol, fs);
 soundsc(x, fs)
 figure
-spectrogram(x, hamming(100), 0, 100, fs)
+spectrogram(x, hamming(100), 0, 100, fs, 'yaxis')
 
+%%
 % adding noise - SNR = 20 dB
 P_sig = var(x);
 P_noise = P_sig / (10^(20/10));    % P_noise = P_sig / SNR = P_sig / 10^(SNR_dB / 10)
 e = randn(size(x))*sqrt(P_noise);  % white noise with P_noise
 xnoisy = x + e;
 figure
-spectrogram(xnoisy, hamming(100), 0, 100, fs)
+spectrogram(xnoisy, hamming(100), 0, 100, fs, 'yaxis')
 
-
+%%
 % adding noise - SNR = -10 dB
 P_sig = var(x);
 P_noise = P_sig / (10^(-10/10));    % P_noise = P_sig / SNR = P_sig / 10^(SNR_dB / 10)
 e = randn(size(x))*sqrt(P_noise);  % white noise with P_noise
 xnoisy = x + e;
 figure
-spectrogram(xnoisy, hamming(100), 0, 100, fs)
+spectrogram(xnoisy, hamming(100), 0, 100, fs, 'yaxis')
 
-
+%%
 % "Ordinary" DFT analysis
 Nsamples = Tsymbol*fs;
 subplot(211), plot(abs(fft(x(1:Nsamples), 20000)))
+hold on;
 subplot(212), plot(abs(fft(x(Nsamples + 1:2*Nsamples), 20000)))
+hold on;
+
+%%
+
+
 % "Ordinary" DFT analysis - noisy
 Nsamples = Tsymbol*fs;
 subplot(211), plot(abs(fft(xnoisy(1:Nsamples), 20000)))
+hold on;
 subplot(212), plot(abs(fft(xnoisy(Nsamples + 1:2*Nsamples), 20000)))
+hold off;
